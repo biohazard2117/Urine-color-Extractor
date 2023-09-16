4459c1993c2cfc633068824d74afe97cf4cb3bee
biohazard2117 <uttu.college.id@gmail.com>
2023-09-16 10:01:37 +0530

    update: changed api endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('urine/', include('urine_extract.urls'))
+    path('api/', include('urine_extract.urls'))
 ]
--- backend/urine_extract/urls.py
+++ backend/urine_extract/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SampleUploadView
 
 urlpatterns = [
-    path('sample/', SampleUploadView.as_view())
+    path('urine/', SampleUploadView.as_view())
 ]
